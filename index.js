6b986d0a5b05500aed9189ee4fad37a63f3dbafa
Saurabh Kumar Bhardwaj <me24.saurabhkumar@gmail.com>
2022-05-30 01:24:49 +0530

    make it better

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 const app = require("./main.route");
 // const app = require("./Routes/user.route");
 const connectToDB = require("./DBConnection/mongoDB");
-const PORT = 9008;
+const PORT = process.env.PORT || 9008;
 http.createServer(app).listen(PORT, () => {
   new connectToDB();
   console.log(`Blog-Api server is running on port no ${PORT}`);
